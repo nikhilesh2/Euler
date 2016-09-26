e595483552672c0d2467b39ffa496b8c4ba9b8ca
nikhilesh2 <nsingh22597@gmail.com>
2016-09-26 02:36:14 -0400

    Delete six.py

--- six.py
+++
@@ -1,9 +0,0 @@
-#find the difference between  the square of the sum and the sum of the squares of the numbers up to 100
-sumOf = 0
-squareOf = 0
-
-for x in range(101):
-	squareOf += x
-	sumOf += x*x
-
-print squareOf*squareOf - sumOf
